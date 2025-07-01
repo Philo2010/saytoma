0795f628c2b665bf93ff4f40c3a8cfabef2f4508
Philo2010 <philipbed52@gmail.com>
2025-07-01 00:02:00 -0700

    I would be better dead

--- src/raw_reader.rs
+++ src/raw_reader.rs
@@ -1,16 +1,41 @@
 //File that reads a file and outputs a struct to read any file by page number
 use std::fs::File;
-use std::io::{Read, Write};
-use std::path::Path;
-use tempfile::tempdir;
+use std::io::{BufReader, BufWriter, copy};
+use std::path::PathBuf;
+use tempfile::TempDir;
 use zip::ZipArchive;
-
 struct PageReader {
     //TODO: Fill Data
+    dir: TempDir,
+    paths: Vec<PathBuf>
 }
 
 impl PageReader {
-    pub fn new(path: &Path) {
-        
+    pub fn new(file: &File) -> Result<PageReader, std::io::Error> {
+        let reader = BufReader::new(file); 
+        let mut archive = ZipArchive::new(reader)?;
+
+        let temp_dir = tempfile::tempdir()?;
+        let mut extracted_paths = Vec::new();
+
+        for i in 0..archive.len() {
+            let mut file_in_zip = archive.by_index(i)?;
+
+            let out_path = temp_dir.path().join(file_in_zip.name());
+
+            if file_in_zip.name().ends_with('/') {
+                std::fs::create_dir_all(&out_path);
+            } else {
+                if let Some(parent) = out_path.parent() {
+                    std::fs::create_dir_all(&out_path);
+                }
+                let out_file = File::create(&out_path)?;
+                let mut writer = BufWriter::new(out_file);
+                copy(&mut file_in_zip, &mut writer)?;
+                extracted_paths.push(out_path);
+            }
+        }
+
+        Ok(PageReader { dir: temp_dir, paths: extracted_paths })
     }
 }
